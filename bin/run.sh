f64f7e3973164c646362b03b26797dcd289a001e
Andrea Gubellini <andreagubellini@gmail.com>
2020-03-11 16:39:35 +0100

    updating with sub_testdir

--- bin/run.sh
+++ bin/run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 if [ "$THREADS" -eq 0 ]; then
-echo "#========! EXECUTING COMMAND robot $ROBOTARGS --outputdir /outputs/ /$TESTDIR/ !========#"
+echo "#========! EXECUTING COMMAND robot $ROBOTARGS --outputdir /outputs/ /$TESTDIR/$SUBTESTDIR !========#"
 xvfb-run -a --server-args="-screen 0 1280x800x24 -ac -nolisten tcp -dpi 96 +extension RANDR" \
-        robot --outputdir /outputs/ /$TESTDIR/
+        robot --outputdir /outputs/ /$TESTDIR/$SUBTESTDIR
 else
-echo "#========! EXECUTING COMMAND pabot $ROBOTARGS --outputdir /outputs/ /$TESTDIR/ !========#"
+echo "#========! EXECUTING COMMAND pabot $ROBOTARGS --outputdir /outputs/ /$TESTDIR/$SUBTESTDIR !========#"
 xvfb-run -a --server-args="-screen 0 1280x800x24 -ac -nolisten tcp -dpi 96 +extension RANDR" \
-        pabot --outputdir /outputs/ /$TESTDIR/
+        pabot --outputdir /outputs/ /$TESTDIR/$SUBTESTDIR
 fi
--- run-docker.sh
+++ run-docker.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 VERSION="latest"
-TESTDIR="atests"
-HOST_TESTDIR=atests
+TESTDIR=tests/
+SUBTESTDIR=mobile_tests
+HOST_TESTDIR=atests/
 ARGS="--console-verbose"
 THREADS="0"
-
 docker run --rm \
            -v $PWD/$HOST_TESTDIR:/$TESTDIR/ \
            -v $PWD/output:/outputs \
            -e ROBOTARGS=$ARGS \
            -e THREADS=$THREADS \
-           -e TESTDIR=$HOST_TESTDIR \
+           -e TESTDIR=$TESTDIR \
+           -e SUBTESTDIR=$SUBTESTDIR \
            agubellini/robotframework-docker:$VERSION
